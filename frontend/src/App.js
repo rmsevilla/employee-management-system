3b65c68c1fe6fcc75d7a503d03c1387660667f8e
rmsevilla <70169981+rmsevilla@users.noreply.github.com>
2022-10-18 22:50:21 -0700

    Created table to list all employees

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import ListEmployeesComponent from './components/ListEmployeesComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1> Hello World! </h1>
-      </header>
+    <div className="container">
+      <ListEmployeesComponent/>
     </div>
   );
 }
---
+++ frontend/src/components/ListEmployeesComponent.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import employeeService from '../services/employeeService';
+
+
+class ListEmployeesComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = { 
+                employees: []
+         }
+    }
+
+    componentDidMount() {
+        employeeService.getAllEmployees().then((res) => {
+            this.setState({employees: res.data});
+        });
+    }
+    render() { 
+        return ( 
+            <div>
+                <h3 className="text-center">Employee List</h3>
+
+                <br></br>
+
+                <table className="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th className="text-center">Employee ID</th>
+                            <th className="text-center">First Name</th>
+                            <th className="text-center">Last Name</th>
+                            <th className="text-center">Job Title</th>
+                            <th className="text-center">Phone Number</th>
+                            <th className="text-center">Email</th>
+                            <th className="text-center">Street</th>
+                            <th className="text-center">City</th>
+                            <th className="text-center">Actions</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {
+                            this.state.employees.map(
+                                employee =>
+                                <tr key = {employee.empId}>
+                                    <td className="text-center">{employee.empId}</td>
+                                    <td className="text-center">{employee.empFirstName}</td>
+                                    <td className="text-center">{employee.empLastName}</td>
+                                    <td className="text-center">{employee.empJobTitle}</td>
+                                    <td className="text-center">{employee.empPhone}</td>
+                                    <td className="text-center">{employee.empEmail}</td>
+                                    <td className="text-center">{employee.empStreet}</td>
+                                    <td className="text-center">{employee.empCity}</td>
+                                </tr>
+                            )
+                        }
+                    </tbody>
+                </table>
+
+            </div>
+        );
+    }
+}
+ 
+export default ListEmployeesComponent;
