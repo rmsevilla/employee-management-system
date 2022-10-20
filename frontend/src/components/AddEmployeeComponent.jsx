4227d459610e82c9be2787e19cda185d8427d9ef
rmsevilla <70169981+rmsevilla@users.noreply.github.com>
2022-10-19 23:44:16 -0700

    Connection to db when adding a new employee

--- frontend/src/components/AddEmployeeComponent.jsx
+++ frontend/src/components/AddEmployeeComponent.jsx
@@ -1,12 +1,83 @@
 import React, { Component } from 'react';
+import employeeService from '../services/employeeService';
 
 class AddEmployeeComponent extends Component {
     constructor(props) {
         super(props)
 
         this.state = { 
+            empId: '',
+            empFirstName: '',
+            empLastName: '',
+            empJobTitle: '',
+            empEmail: '',
+            empPhone: '',
+            empStreet: '',
+            empCity: ''
          }
+
+         this.empIdHandler = this.empIdHandler.bind(this);
+         this.firstNameHandler = this.firstNameHandler.bind(this);
+         this.lastNameHandler = this.lastNameHandler.bind(this);
+         this.empJobTitleHandler = this.empJobTitleHandler.bind(this);
+         this.empPhoneHandler = this.empPhoneHandler.bind(this);
+         this.empEmailHandler = this.empEmailHandler.bind(this);
+         this.empStreetHandler = this.empStreetHandler.bind(this);
+         this.empCityHandler = this.empCityHandler.bind(this);
+         this.saveEmployee = this.saveEmployee.bind(this);
     }
+
+    empIdHandler= (event) => {
+        this.setState({empId: event.target.value});
+    }
+
+    firstNameHandler= (event) => {
+        this.setState({empFirstName: event.target.value});
+    } 
+
+    lastNameHandler= (event) => {
+        this.setState({empLastName: event.target.value});
+    } 
+
+    empJobTitleHandler= (event) => {
+        this.setState({empJobTitle: event.target.value});
+    }
+
+    empPhoneHandler= (event) => {
+        this.setState({empPhone: event.target.value});
+    }
+
+    empEmailHandler= (event) => {
+        this.setState({empEmail: event.target.value});
+    }
+
+    empStreetHandler= (event) => {
+        this.setState({empStreet: event.target.value});
+    }
+
+    empCityHandler= (event) => {
+        this.setState({empCity: event.target.value});
+    }
+
+    saveEmployee = (e) => {
+        e.preventDefault();
+        let employee = {empId: this.state.empId, empFirstName: this.state.empFirstName, empLastName: this.state.empLastName,
+                        empJobTitle: this.state.empJobTitle, empPhone: this.state.empPhone, empEmail: this.state.empEmail, empCity: this.state.empCity, empStreet: this.state.empStreet};
+        console.log('employee => ' + JSON.stringify(employee));
+        alert("Employee added");
+        
+        employeeService.addEmployee(employee).then(res => {
+            window.location.reload(false);
+        });
+
+        this.setState({empId: "", empFirstName:"", empLastName: "", empJobTitle: "", empPhone: "", empEmail: "", empStreet: "", empCity: ""});
+    }
+
+    cancel = () => {
+        this.setState({empId: "", empFirstName:"", empLastName: "", empJobTitle: "", empPhone: "", empEmail: "", empStreet: "", empCity: ""});
+    }
+
+
 
     render() { 
         return ( 
@@ -27,42 +98,42 @@ class AddEmployeeComponent extends Component {
                     <div class="modal-body">
                     <form>
                         <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Employee ID</label>
-                            <input type="number" class="form-control" id=""></input>
+                            <label for="empId" class="form-label">Employee ID</label>
+                            <input type="number" class="form-control" value={this.state.empId} onChange={this.empIdHandler}></input>
                         </div>
                         <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">First Name</label>
-                            <input type="text" class="form-control" id=""></input>
+                            <label for="empFirstName" class="form-label">First Name</label>
+                            <input type="text" class="form-control" value={this.state.empFirstName} onChange={this.firstNameHandler}></input>
                         </div>
                         <div class="mb-3">
-                            <label for="exampleInputPassword1" class="form-label">Last Name</label>
-                            <input type="text" class="form-control" id=""></input>
+                            <label for="empLastName" class="form-label">Last Name</label>
+                            <input type="text" class="form-control" value={this.state.empLastName} onChange={this.lastNameHandler}></input>
                         </div>
                         <div class="mb-3">
-                            <label for="exampleInputPassword1" class="form-label">Job Title</label>
-                            <input type="text" class="form-control" id=""></input>
+                            <label for="empJobTitle" class="form-label">Job Title</label>
+                            <input type="text" class="form-control" value={this.state.empJobTitle} onChange={this.empJobTitleHandler}></input>
                         </div>
                         <div class="mb-3">
-                            <label for="exampleInputPassword1" class="form-label">Phone Number</label>
-                            <input type="tel" class="form-control" id=""></input>
+                            <label for="empPhone" class="form-label">Phone Number</label>
+                            <input type="tel" class="form-control" value={this.state.empPhone} onChange={this.empPhoneHandler}></input>
                         </div>
                         <div class="mb-3">
-                            <label for="exampleInputPassword1" class="form-label">Email</label>
-                            <input type="email" class="form-control" id=""></input>
+                            <label for="empEmail" class="form-label">Email</label>
+                            <input type="email" class="form-control" value={this.state.empEmail} onChange={this.empEmailHandler}></input>
                         </div>
                         <div class="mb-3">
-                            <label for="exampleInputPassword1" class="form-label">Street</label>
-                            <input type="text" class="form-control" id=""></input>
+                            <label for="empStreet" class="form-label">Street</label>
+                            <input type="text" class="form-control" value={this.state.empStreet} onChange={this.empStreetHandler}></input>
                         </div>
                         <div class="mb-3">
-                            <label for="exampleInputPassword1" class="form-label">City</label>
-                            <input type="text" class="form-control" id=""></input>
+                            <label for="empCity" class="form-label">City</label>
+                            <input type="text" class="form-control" value={this.state.empCity} onChange={this.empCityHandler}></input>
                         </div>
                         </form>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" onClick={this.cancel.bind(this)}>Cancel</button>
+                        <button type="button" class="btn btn-success" onClick={this.saveEmployee}>Add Employee</button>
                     </div>
                     </div>
                 </div>
--- frontend/src/services/employeeService.js
+++ frontend/src/services/employeeService.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
-const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/employees/getAllEmployees";
+const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/employees";
 
 class EmployeeService {
 
     getAllEmployees() {
-        return axios.get(EMPLOYEE_API_BASE_URL)
+        return axios.get(EMPLOYEE_API_BASE_URL + "/getAllEmployees")
+    }
+
+    addEmployee(employee) {
+        return axios.post(EMPLOYEE_API_BASE_URL + "/addEmployee", employee)
     }
 
 }
