baddf8c4505ca3c6dfe1cebea12fae4bc814fec8
rmsevilla <70169981+rmsevilla@users.noreply.github.com>
2022-11-01 15:26:31 -0700

    Changed formatting of search bar

--- frontend/src/components/HeaderComponent.jsx
+++ frontend/src/components/HeaderComponent.jsx
@@ -6,8 +6,9 @@ class HeaderComponent extends Component {
         super(props)
 
         this.state = { 
-         }
+         };
     }
+    
 
     render() { 
         return ( 
@@ -15,23 +16,22 @@ class HeaderComponent extends Component {
                 <header>
                     <nav className="navbar navbar-expand-lg">
                         <div className="container-fluid">
-                            <a className="navbar-brand" href="#">Employee Management System</a>
+                            <a className="navbar-brand ms-5" href="#">Employee Management System</a>
+
+                                <form class="d-flex w-50" id="search-form" role="search">
+                                        <input class="form-control" type="search" placeholder="Search" aria-label="Search"></input>
+                                        <button class="btn btn-outline-success" id="search-btn" type="submit">Search</button>
+                                </form>
+
                                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                                     <span className="navbar-toggler-icon"></span>
                                 </button>
                                 <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                        <li className="nav-item">
-                                            <a className="nav-link" aria-current="page" href="#">Home</a>
-                                        </li>
+                                    <ul className="navbar-nav ms-auto me-5">
                                         <li className="nav-item">
                                             <a className="nav-link" href="#">Sign Out</a>
                                         </li>
                                     </ul>
-                                    <form class="d-flex" role="search">
-                                         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-                                        <button class="btn btn-outline-success" type="submit">Search</button>
-                                    </form>
                                 </div>
                         </div>
                     </nav>
